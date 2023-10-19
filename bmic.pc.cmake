1185ae0c0f71a91907e23d5d0651b8155d8361aa
Mark Piper <mark.piper@colorado.edu>
2023-10-19 14:14:18 -0600

    Use GNUInstallDirs and standard install variables

--- bmic.pc.cmake
+++ bmic.pc.cmake
@@ -1,5 +1,5 @@
 Name: Bmi
 Description: The Basic Model Interface
 Version: ${BMI_VERSION}
-Libs: -L${CMAKE_INSTALL_PREFIX}/lib -lbmic
-Cflags: -I${CMAKE_INSTALL_PREFIX}/include
+Libs: -L${CMAKE_INSTALL_LIBDIR} -lbmic
+Cflags: -I${CMAKE_INSTALL_INCLUDEDIR}

3f98f512636ed8eecbfa99230f3cf7678502d7ff
Mark Piper <mark.piper@colorado.edu>
2023-10-24 14:56:28 -0600

    Improve information in pkg-config file

--- bmic.pc.cmake
+++ bmic.pc.cmake
@@ -1,5 +1,6 @@
-Name: Bmi
-Description: The Basic Model Interface
+Name: bmi-c
+Description: The Basic Model Interface for C
+URL: https://bmi.readthedocs.io
 Version: ${BMI_VERSION}
-Libs: -L${CMAKE_INSTALL_LIBDIR} -lbmic
+Libs: -L${CMAKE_INSTALL_LIBDIR} -l${CMAKE_PROJECT_NAME}
 Cflags: -I${CMAKE_INSTALL_INCLUDEDIR}

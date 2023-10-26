cb402a63ffeaf066415da67d65e7cbca38fac0b8
Mark Piper <mark.piper@colorado.edu>
2023-10-26 11:04:36 -0600

    Use full path CMake variables (#10)

--- bmic.pc.cmake
+++ bmic.pc.cmake
@@ -2,5 +2,5 @@ Name: bmi-c
 Description: The Basic Model Interface for C
 URL: https://bmi.readthedocs.io
 Version: ${BMI_VERSION}
-Libs: -L${CMAKE_INSTALL_LIBDIR} -l${CMAKE_PROJECT_NAME}
-Cflags: -I${CMAKE_INSTALL_INCLUDEDIR}
+Libs: -L${CMAKE_INSTALL_FULL_LIBDIR} -l${CMAKE_PROJECT_NAME}
+Cflags: -I${CMAKE_INSTALL_FULL_INCLUDEDIR}
